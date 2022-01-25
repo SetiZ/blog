d2958908ace960a7c7f95e9d50be78ee4167d5f1
SetiZ <funkysetiz@gmail.com>
2022-01-25 17:21:05 +0100

    change site

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default /** @type {import('astro').AstroUserConfig} */ ({
 	// Enable the Preact renderer to support Preact JSX components.
 	renderers: ['@astrojs/renderer-preact'],
 	buildOptions: {
-		site: 'https://setiz.github.io/',
+		site: 'https://setiz.github.io/blog/',
 		sitemap: true,
 	}
 });
