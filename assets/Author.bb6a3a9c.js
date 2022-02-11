e8cfe85bd72ba4620293b0ddcdd2de04764ac29a
SetiZ <SetiZ@users.noreply.github.com>
2022-02-11 21:23:03 +0000

    Deploy https://github.com/SetiZ/blog/commit/a8d65d2c3ad4713ef4d0f67cd50a77098a4393ff

--- assets/Author.bb6a3a9c.js
+++
@@ -1,7 +0,0 @@
-import{c as d,a as i,b as h,r as m,d as p}from"./vendor.5ae2b8e3.js";d("/src/components/Author.astro",{modules:[],hydratedComponents:[],hydrationDirectives:new Set([]),hoisted:[]});const l=i("/src/components/Author.astro","https://setiz.github.io/blog/","file:///home/runner/work/blog/blog/");h(async(t,o,r)=>{const s=t.createAstro(l,o,r),{name:a,href:e}=s.props,n=[{props:{"data-astro-id":"KO5QTRDP"},children:".author.astro-KO5QTRDP{margin-bottom:0.75rem;}"}];for(const c of n)t.styles.add(c);return m`
-<div class="author astro-KO5QTRDP">
-	<p class="astro-KO5QTRDP"><a${p(e,"href")} class="astro-KO5QTRDP">${a}</a></p>
-</div>
-
-
-`});
